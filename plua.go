f5f26eb1abbcccca0f211909aec72b33732fd160
esrrhs <esrrhs@163.com>
2020-08-15 18:19:04 +0800

    add

--- plua.go
+++ plua.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"flag"
 	"fmt"
+	"github.com/goccy/go-graphviz"
 	"io/ioutil"
 	"os"
 	"sort"
+	"strings"
 )
 
 const (
@@ -30,6 +33,9 @@ func main() {
 
 	input := flag.String("i", "", "input file")
 	text := flag.Bool("text", false, "show text result")
+	dot := flag.Bool("dot", false, "show dot result")
+	png := flag.String("png", "", "gen png file")
+	svg := flag.String("svg", "", "gen svg file")
 
 	flag.Parse()
 
@@ -47,6 +53,13 @@ func main() {
 		showtext(filedata)
 	}
 
+	if *dot {
+		showdot(filedata)
+	}
+
+	if *png != "" || *svg != "" {
+		showpng(filedata, *png, *svg)
+	}
 }
 
 func parse(filename string) (*FileData, bool) {
@@ -174,5 +187,152 @@ func showtext(filedata *FileData) {
 
 	for _, ar := range funcarr {
 		fmt.Printf("%v%%\t%v\n", ar.count*100/total, filedata.id2str[ar.id])
+	}
+}
+
+func escape_dot_name(name string) string {
+	return strings.Replace(name, "\"", "'", -1)
+}
+
+func gendot(filedata *FileData) string {
+
+	ret := ""
+
+	totalself := 0
+	funcmapself := make(map[int]int)
+	for _, cs := range filedata.callstack {
+		funcmapself[cs.stacks[cs.deps-1]] += cs.count
+		totalself += cs.count
+	}
+
+	total := 0
+	funcmap := make(map[int]int)
+	for _, cs := range filedata.callstack {
+		for i := 0; i < cs.deps; i++ {
+			funcmap[cs.stacks[i]] += cs.count
+		}
+		total += cs.count
+	}
+
+	type arr struct {
+		id    int
+		count int
+	}
+	funcarr := make([]arr, 0)
+	for k, v := range funcmap {
+		funcarr = append(funcarr, arr{k, v})
+	}
+
+	sort.Slice(funcarr, func(i, j int) bool {
+		return funcarr[j].count < funcarr[i].count
+	})
+
+	type pair struct {
+		first  int
+		second int
+	}
+	hassonset := make(map[int]int)
+	funccallset := make(map[pair]int)
+	for _, cs := range filedata.callstack {
+		for i := 0; i < cs.deps-1; i++ {
+			funccallset[pair{cs.stacks[i], cs.stacks[i+1]}]++
+			hassonset[cs.stacks[i]]++
+		}
+	}
+
+	ret += fmt.Sprintf("digraph G {\n")
+
+	for _, ar := range funcarr {
+		ret += fmt.Sprintf("\tnode%v [label=\"%v\\r%v (%v%%)\\r",
+			ar.id, escape_dot_name(filedata.id2str[ar.id]), funcmapself[ar.id], funcmapself[ar.id]*100/totalself)
+
+		_, ok := hassonset[ar.id]
+		if ok {
+			ret += fmt.Sprintf("%v (%v%%)\\r", ar.count, ar.count*100/total)
+		}
+
+		fontsize := funcmapself[ar.id] * 100 / totalself
+		if fontsize < 10 {
+			fontsize = 10
+		}
+
+		ret += fmt.Sprintf("\";")
+		ret += fmt.Sprintf("fontsize=%v", fontsize)
+		ret += fmt.Sprintf(";shape=box;")
+		ret += fmt.Sprintf("];\n")
+	}
+
+	for k, _ := range funccallset {
+		linewidth := float64(funcmap[k.second]) * 3.0 / float64(total)
+		if linewidth < 0.5 {
+			linewidth = 0.5
+		}
+		ret += fmt.Sprintf("\tnode%v->node%v [style=\"setlinewidth(%v)\" label=%v];\n", k.first, k.second, linewidth, funcmap[k.second])
+	}
+
+	ret += fmt.Sprintf("}\n")
+
+	return ret
+}
+
+func showdot(filedata *FileData) {
+	fmt.Print(gendot(filedata))
+}
+
+func showpng(filedata *FileData, png string, svg string) {
+	dot := gendot(filedata)
+
+	graph, err := graphviz.ParseBytes([]byte(dot))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if png != "" {
+		g := graphviz.New()
+
+		// 1. write encoded PNG data to buffer
+		var buf bytes.Buffer
+		if err := g.Render(graph, graphviz.PNG, &buf); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// 2. get as image.Image instance
+		_, err = g.RenderImage(graph)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// 3. write to file directly
+		if err := g.RenderFilename(graph, graphviz.PNG, png); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	if svg != "" {
+		g := graphviz.New()
+
+		// 1. write encoded PNG data to buffer
+		var buf bytes.Buffer
+		if err := g.Render(graph, graphviz.SVG, &buf); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// 2. get as image.Image instance
+		_, err = g.RenderImage(graph)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// 3. write to file directly
+		if err := g.RenderFilename(graph, graphviz.SVG, svg); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 }
