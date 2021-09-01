f4f2061f9f7c140e76523e042321973f15a48d7d
patrick <506765973@qq.com>
2021-09-01 14:55:22 +0800

    [Mod] project construction

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #! /bin/sh
-rm CMakeCache.txt -rf
-cmake .
-make clean
+dir=$(cd `dirname $0`;pwd)
+projectdir=$dir/
+builddir="$projectdir/build"
+rundir="$projectdir/bin"
+
+if [ ! -d "$rundir" ]; then
+  mkdir -p $rundir && cd $rundir
+fi
+
+if [ -d "$builddir" ]; then
+  rm $builddir -rf
+  mkdir -p $builddir && cd $builddir
+  cd $builddir
+else
+  mkdir -p $builddir && cd $builddir
+fi
+
+cmake ../
 make
